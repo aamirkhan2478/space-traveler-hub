99f0808473d0579db47eab6646db92006dff9151
soulemanou kunchiefuh babanou <57832090+soulemanou-software@users.noreply.github.com>
2022-09-30 15:16:00 +0100

    Implimented the joining mission feature

--- src/Layout/pages/MissionPage.jsx
+++ src/Layout/pages/MissionPage.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import Table from 'react-bootstrap/Table';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/Button';
+import { joinedMissions, leavedMissions } from '../../redux/Missions/missions';
 
 const MissionPage = () => {
   const { missions } = useSelector(
     (state) => state.mission,
   );
+  const dispatch = useDispatch();
+  const clickHandler = (id, joined) => {
+    if (joined === false || joined === undefined) {
+      dispatch(joinedMissions(id));
+    } else {
+      dispatch(leavedMissions(id));
+    }
+  };
   console.log(missions);
   return (
     <Table striped bordered hover>
@@ -26,13 +35,21 @@ const MissionPage = () => {
               {' '}
               {mission.mission_name}
             </td>
-            <td>{mission.description}</td>
+            <td>{mission.description.slice(0, 150)}</td>
             <td>
               {' '}
-              <Badge bg="secondary">New</Badge>
+              <Badge bg={mission.joined ? 'primary' : 'secondary'}>
+                {mission.joined ? 'ACTIVE MEMBER' : 'NOT A MEMBER '}
+              </Badge>
             </td>
             <td>
-              <Button variant="primary">Primary</Button>
+              <Button
+                variant={mission.joined ? 'outline-danger' : 'outline-secondary'}
+                onClick={() => clickHandler(mission.mission_id, mission.joined)}
+              >
+                {' '}
+                {mission.joined ? 'Leave Mission' : 'Join Mission'}
+              </Button>
               {' '}
             </td>
           </tr>
