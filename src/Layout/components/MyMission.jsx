038720ecd5b79886a544023213acf296bdeae273
soulemanou kunchiefuh babanou <57832090+soulemanou-software@users.noreply.github.com>
2022-09-30 15:21:25 +0100

    implemented the render joined mission on the profile page

--- src/Layout/components/MyMission.jsx
+++ src/Layout/components/MyMission.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { ListGroup } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 
-const MyMission = () => (
-  <>
-    <h3>My Mission</h3>
-  </>
-);
+const MyMission = () => {
+  const { missions } = useSelector((state) => state.mission);
+  return (
+    <>
+      <h3>My Mission</h3>
+      <ListGroup>
+        {missions
+          .map(
+            (mission) => mission.joined && (
+            <ListGroup.Item>{mission.mission_name}</ListGroup.Item>
+            ),
+          )}
+      </ListGroup>
+    </>
+  );
+};
 
 export default MyMission;
--- src/Layout/pages/ProfilePage.jsx
+++ src/Layout/pages/ProfilePage.jsx
@@ -3,20 +3,17 @@ import { Col, Container, Row } from 'react-bootstrap';
 import MyMission from '../components/MyMission';
 import MyRockets from '../components/MyRockets';
 
-const ProfilePage = () => {
-  document.title = "Space Traveler's Hub| Profile";
-  return (
-    <Container className="my-5">
-      <Row>
-        <Col md={6}>
-          <MyMission />
-        </Col>
-        <Col md={6}>
-          <MyRockets />
-        </Col>
-      </Row>
-    </Container>
-  );
-};
+const ProfilePage = () => (
+  <Container className="my-5">
+    <Row>
+      <Col md={6}>
+        <MyMission />
+      </Col>
+      <Col md={6}>
+        <MyRockets />
+      </Col>
+    </Row>
+  </Container>
+);
 
 export default ProfilePage;
