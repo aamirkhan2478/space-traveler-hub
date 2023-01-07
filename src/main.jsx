d66d6ca585e678203106c732173da06eea923c0f
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-01-08 00:52:03 +0500

    Change HashRouter to BrowserRouter and also _redirects file for deploying

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -10,7 +10,7 @@ import store from "./redux/configureStore";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router basename={import.meta.env.PUBLIC_URL}>
+      <Router>
         <App />
       </Router>
     </Provider>
